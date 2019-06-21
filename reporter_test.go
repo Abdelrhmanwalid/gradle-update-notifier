ec90d48f7f2c42b8c4f15ab8e770f9eaa2aca21b
Abdelrhman Walid <abdelrhmanwalid1@gmail.com>
2019-06-22 01:38:33 +0200

    fix test cases and add project url to test data

--- reporter_test.go
+++ reporter_test.go
@@ -8,10 +8,9 @@ import (
 func TestGenerateIssueBody(t *testing.T) {
 	jsonData, _ := ioutil.ReadFile("testdata/normal.json")
 	report, _ := parse(jsonData, nil)
-	releaseNotes := []ReleaseNote{ReleaseNote{Package: "com.google.firebase:firebase-core", URL: "https://firebase.google.com/support/releases"}, ReleaseNote{Package: "com.github.ben-manes:gradle-version-plugin", URL: ""}}
 
-	issueBody := generateIssueBody(report, releaseNotes)
-	expected := "* [ ] `com.github.ben-manes:gradle-versions-plugin:0.13.0`([Release Note](https://firebase.google.com/support/releases))\n* [ ] `com.google.firebase:firebase-core:9.4.0`\n"
+	issueBody := generateIssueBody(report)
+	expected := "* [ ] `com.github.ben-manes:gradle-versions-plugin:0.13.0`([Url](https://github.com/ben-manes/gradle-versions-plugin))\n* [ ] `com.google.firebase:firebase-core:9.4.0`\n"
 
 	if issueBody != expected {
 		t.Errorf("Expect \n%v\n, but \n%v", expected, issueBody)
